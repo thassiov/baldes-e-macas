601b57d079a8ab93e79d43299e1d9e52eae42313
thassiov <tvmcarvalho@gmail.com>
2024-01-30 20:01:24 -0300

    Atualiza roteador com nova rota para listar baldes

--- src/rest-api/routers/index.ts
+++ src/rest-api/routers/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import { Services } from '../../utils/types';
 import { createBaldeHandlerFactory } from './createBalde';
+import { listBaldesHandlerFactory } from './listBaldes';
 
 const router = express.Router();
 
@@ -9,6 +10,7 @@ router.use('/v1');
 
 function setRouter(services: Services): express.Router {
   router.post('/baldes', createBaldeHandlerFactory(services.balde));
+  router.get('/baldes', listBaldesHandlerFactory(services.balde));
 
   return router;
 }
