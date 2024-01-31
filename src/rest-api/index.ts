bf1dbce81e3d1edba262a23417f138a667a93243
thassiov <tvmcarvalho@gmail.com>
2024-01-31 17:23:28 -0300

    Serve o openapi spec no endpoint /api-docs

--- src/rest-api/index.ts
+++ src/rest-api/index.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import { readFile } from 'node:fs/promises';
+import { resolve } from 'node:path';
 import pinoHttp from 'pino-http';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'yaml';
 
 import { configs } from '../utils/configs';
 import { logger } from '../utils/logger';
@@ -12,15 +16,22 @@ api.use(express.json());
 api.use(express.urlencoded());
 api.use(pinoHttp());
 
-function startApi(services: Services) {
+async function startApi(services: Services): Promise<void> {
   const router = setRouter(services);
   api.use('/api/v1', router);
 
-  api.listen(configs.API_PORT, () =>
+  const path = resolve('openapi-spec.yaml');
+  const file = await readFile(path, 'utf8');
+  const swaggerDocument = yaml.parse(file);
+  api.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+  api.listen(configs.API_PORT, () => {
+    const endereco = `http://0.0.0.0:${configs.API_PORT}`;
+    logger.info(`API REST iniciada no endereco ${endereco}/api/v1`);
     logger.info(
-      `Servidor initiaco no endereco http://0.0.0.0:${configs.API_PORT}`
-    )
-  );
+      `Para acessar a documentacao da API, acesse ${endereco}/api-docs`
+    );
+  });
 }
 
 export { startApi };
