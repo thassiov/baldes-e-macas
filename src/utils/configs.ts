ecb8b34667e5d759041570cdaa01355b1dd83f07
thassiov <tvmcarvalho@gmail.com>
2024-01-31 18:50:48 -0300

    Adiciona docker-compose

--- src/utils/configs.ts
+++ src/utils/configs.ts
@@ -3,7 +3,7 @@ import { config } from 'dotenv';
 config();
 
 const configs = {
-  DB_FILE: process.env.DB_FILE || './database.sqlite3',
+  DB_FILE: process.env.DB_FILE || '/data/database.sqlite3',
   API_PORT: process.env.API_PORT || 8080,
   MACA_EVICTION_INTERVAL_CHECK: 500,
 };
